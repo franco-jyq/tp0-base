a17216633d9f50a64172506589cb7ddced9e2601
franco-jyq <fgazzola@fi.uba.ar>
2024-09-04 17:35:17 -0300

    qf: eliminate validations

--- generate-compose.py
+++ generate-compose.py
@@ -5,12 +5,6 @@ FILENAME = sys.argv[1]
 NUM_CLIENTS = sys.argv[2]
 
 
-# Check if the number is a valid positive integer
-if not NUM_CLIENTS.isdigit() or int(NUM_CLIENTS) < 0:
-    print("Error: NUM_CLIENTS must be a positive integer.", file=sys.stderr)
-    sys.exit(1)
-
-
 # Define the static part of the docker-compose file
 docker_compose_template = """
 version: '3'
